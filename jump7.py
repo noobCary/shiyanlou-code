7c0a2e8268fa529c0e71e96dd7b6c429f54b9733
noobCary <1225736949@qq.com>
2019-08-05 15:09:47 +0800

    实验楼逼我的

---
+++ jump7.py
@@ -0,0 +1,15 @@
+
+i = 1
+while i <= 100:
+    i = i + 1
+    if i == 7:
+        continue
+    elif i % 10 == 7:
+        continue
+    elif i % 7 == 0:
+        continue
+    elif i // 10 == 7:
+        continue
+    else:
+        print (i)
+    
